cb8b0ffb0c2b37165c4f5063fc767fb990061f9d
Caitlin Ross <rossc3@rpi.edu>
2015-10-26 02:41:29 -0400

    finished assignment 4

--- assignment4/assignment4.py
+++ assignment4/assignment4.py
@@ -11,12 +11,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 from Bio import SeqIO
 
+# this function performs the main loops in Gibbs sampling algorithm
+# isBurnin is a boolean to determine whether to perform the last section in the loop
 def sampler(numIterations, isBurnin):
     theta_ij = {}
-    theta_ij["A"] = []
-    theta_ij["C"] = []
-    theta_ij["G"] = []
-    theta_ij["T"] = []
+    theta_ij["A"] = [0,0,0,0,0,0,0,0]
+    theta_ij["C"] = [0,0,0,0,0,0,0,0]
+    theta_ij["G"] = [0,0,0,0,0,0,0,0]
+    theta_ij["T"] = [0,0,0,0,0,0,0,0]
 
     theta_Bj = {}
     theta_Bj["A"] = 0
@@ -30,25 +32,27 @@ def sampler(numIterations, isBurnin):
     counts["C"] = 0
     counts["T"] = 0
     for itr in range(numIterations):
+
         for i in range(len(all_seqs)):
             a[i] = 0
-            
-
+        
             # calculate theta of the motif by counting nucleotides in each position
+            # calculate this using all other DNA sequences
             for pos in range(w):
                 for j in range(len(all_seqs)):
                     if not i == j:
                         counts[all_seqs[j][a[j]+pos]] += 1
-                theta_ij["A"].append((float(counts["A"])+ 1) / (sum(counts.values()) + 4))
-                theta_ij["G"].append((float(counts["G"])+ 1) / (sum(counts.values()) + 4))
-                theta_ij["C"].append((float(counts["C"])+ 1) / (sum(counts.values()) + 4))
-                theta_ij["T"].append((float(counts["T"])+ 1) / (sum(counts.values()) + 4))
+                theta_ij["A"][pos] = (float(counts["A"])+ 1) / (sum(counts.values()) + 4)
+                theta_ij["G"][pos] = (float(counts["G"])+ 1) / (sum(counts.values()) + 4)
+                theta_ij["C"][pos] = (float(counts["C"])+ 1) / (sum(counts.values()) + 4)
+                theta_ij["T"][pos] = (float(counts["T"])+ 1) / (sum(counts.values()) + 4)
                 counts["A"] = 0
                 counts["G"] = 0
                 counts["C"] = 0
                 counts["T"] = 0
                 
             # calculate theta of background by counting nucleotides
+            # calculate this using all other DNA sequences
             for pos in range(len(all_seqs[0])):
                 for j in range(len(all_seqs)):
                     if not i == j:
@@ -63,7 +67,7 @@ def sampler(numIterations, isBurnin):
             counts["T"] = 0
 
             r = []
-            # calculate P(x) for each theta
+            # calculate P(x) for each theta and use it to find r
             for pos in range(len(all_seqs[0])-w):
                 pMotif = 1
                 pBack = 1
@@ -80,10 +84,12 @@ def sampler(numIterations, isBurnin):
             tmpA = np.random.choice(len(all_seqs[0])-w, 1, p=r)
             a[i] = tmpA[0]
 
+            # for the sampling loop only
             if not isBurnin:
                 c[i][a[i]] += 1
 
-                if i == numIterations - 1:
+                # only need thetas from final iteration
+                if itr == numIterations - 1:
                     for j in range(w):
                         allTMotif["A"][j] += theta_ij["A"][j]
                         allTMotif["C"][j] += theta_ij["C"][j]
@@ -133,8 +139,8 @@ def Main():
     allTBack["G"] = 0
     allTBack["C"] = 0
     allTBack["T"] = 0
-    burnin = 10
-    sampling = 20
+    burnin = 1000
+    sampling = 2000
 
     # run 5 chains
     for i in range(5):
@@ -143,11 +149,6 @@ def Main():
         sampler(burnin, True)
         sampler(sampling, False)
 
-    for i in range(len(c)):
-        for j in range(len(c)):
-            c[i][j] /= sampling*(i+1)
-
-    print(allTMotif)
     for i in range(w):
         total = allTMotif["A"][i] + allTMotif["G"][i] + allTMotif["C"][i] + allTMotif["T"][i]
         allTMotif["A"][i] /= total
@@ -156,10 +157,10 @@ def Main():
         allTMotif["T"][i] /= total
 
     total = allTBack["A"] + allTBack["G"] + allTBack["C"] + allTBack["T"]
-    allTBack["A"][i] /= total
-    allTBack["C"][i] /= total
-    allTBack["G"][i] /= total
-    allTBack["T"][i] /= total
+    allTBack["A"] /= total
+    allTBack["C"] /= total
+    allTBack["G"] /= total
+    allTBack["T"] /= total
 
     # print the results
     print("Motif Model\n")
@@ -167,24 +168,26 @@ def Main():
 
     print("Background Model:\n")
     print(allTBack)
+    
+    for i in range(len(c)):
+        for j in range(len(c[i])):
+            c[i][j] /= (sampling*5)
 
-    print("\nlocations\n")
+    print("\nSites:\n")
     for i in range(len(all_seqs)):
-        for j in range(len(all_seqs[i] - w)):
+        for j in range(len(all_seqs[i])-w):
             if (c[i][j] > .5):
-                print("Sequence " + str(i+1) + "\n")
-                print("pos: " + str(j+1) + "\n")
-                print("seq: " + all_seqs[i][j:j+w] + "\n")
-                print("samp freq: " + str(c[i][j]) + "\n")
+                print(str(j+1) + " " + all_seqs[i][j:j+w] + " " + str(c[i][j]) + "\n")
     
     # create graphs
     x = np.arange(1, 93)
     for i in range(len(all_seqs)):
         plt.bar(x, c[i])
         plt.xlabel("Position")
-        plt.ylable("Probability")
+        plt.ylabel("Probability")
         plt.title("Probability of each position being sampled for Seq " + str(i+1))
-        plt.show()
+        plt.savefig("plot"+str(i+1)+".pdf", format="pdf")
+        #plt.show()
 
 
 if __name__ == '__main__':
