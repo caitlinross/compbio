3406acc1e2332c276888ea542f1ca918f3ce2b90
Caitlin Ross <rossc3@rpi.edu>
2015-10-28 11:43:43 -0400

    working on mutual information matrix

--- assignment5/assignment5.py
+++ assignment5/assignment5.py
@@ -24,8 +24,46 @@ def consensusSeq(alignment):
             counts[alignment[i][j]] += 1
         con_seq += max(counts, key=lambda k: counts[k])
     return con_seq
+    
+def mutualInformation(alignment):
+    matrix = []
+    counts = []
+    for i in range(5):
+        counts.append([])
+        for j in range(len(alignment[0])):
+            counts[i].append(0)
+    for i in range(len(alignment[0])):
+        matrix.append([])
+        for j in range(len(alignment[0])):
+            matrix[i].append(0)
+            counts[alphabet[alignment[i][j]]][j] += 1
+            
+    for i in range(len(counts)):
+        #print(counts[i])
+        for j in range(len(counts[i])):
+            counts[i][j] /= len(alignment)
+        #print(counts[i])
+        
+    # this is probably wrong
+    for row in range(len(alignment[0])):
+        for col in range(len(alignment[0])):
+            total = 0
+            for i in range(len(counts)):
+                for j in range(len(counts)):
+                    dinuc = rev_alph[i]+rev_alph[j]
+                    if dinuc in ('AU', 'UA', 'CG', 'GC', 'GU', 'UG'):
+                        total += ((counts[alphabet[dinuc[0]]][i]+counts[alphabet[dinuc[1]]][j]))
+                        
+                            
+                        
+                
+            
+    return matrix
 
 def Main():
+    global alphabet, rev_alph
+    alphabet = {'A': 0, 'C':1, 'G': 2, 'U':3, '-':4}
+    rev_alph = {0:'A', 1:'C', 2:'G', 3:'U', 4:'-'}
     # read from clustal file
     alignment = []
     f = open("muscle-align.clwstrict")
@@ -35,6 +73,10 @@ def Main():
 
     con_seq = consensusSeq(alignment)
     print("consensus sequence: " + con_seq)
+    
+    miMatrix = mutualInformation(alignment)
+    print("Mutual Information matrix")
+    print(miMatrix)
 
 if __name__ == '__main__':
     Main()
