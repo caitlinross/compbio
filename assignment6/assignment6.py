b0d905e9b0cb5fe8d6e9417a947cfca91aba1299
Caitlin Ross <rossc3@rpi.edu>
2015-11-19 17:17:18 -0500

    cleaned up code and comments

--- assignment6/assignment6.py
+++ assignment6/assignment6.py
@@ -8,8 +8,9 @@
 import math
 from Bio import Phylo
 
-def felsenstein(n, root):
-    k = 2*n-1
+# takes in a tree node and recurses through to find the probability
+# for each nucleotide at each node
+def felsenstein(root):
     P_Lk = {}
     P_Lk["A"] = 0
     P_Lk["C"] = 0
@@ -18,15 +19,14 @@ def felsenstein(n, root):
       
     if root.is_terminal():
         P_Lk[str(root)[-1]] = 1
-        print(root)
-        
+        print("probability at leaf " + str(root)[-1] + " " + str(P_Lk))
     else:
-        Phylo.draw_ascii(root) 
         [leftChild, rightChild] = root.clades
-        newPlk1 = felsenstein(n-1, leftChild)
-        St1 = jukesCantor(root.distance(leftChild))
-        newPlk2 = felsenstein(n-1, rightChild)
-        St2 = jukesCantor(root.distance(rightChild))
+        newPlk1 = felsenstein(leftChild)
+        St1 = jukesCantor(leftChild.branch_length)
+        newPlk2 = felsenstein(rightChild)
+        St2 = jukesCantor(rightChild.branch_length)        
+
         
         for i in range(4):
             tmp1 = 0
@@ -36,10 +36,10 @@ def felsenstein(n, root):
             for c in range(4):
                 tmp2 += St2[c][i] * newPlk2[rev_alphabet[c]] 
             P_Lk[rev_alphabet[i]] = tmp1 * tmp2
-            print(rev_alphabet[i] + ": " + str(P_Lk[rev_alphabet[i]]))
-     
+        print("probability at node " + str(P_Lk))
     return P_Lk
     
+# finds Jukes-Cantor substitution matrix given the branch length
 def jukesCantor(branchLen):
     alpha = math.pow(10,-9)
     t = branchLen * 1000000
@@ -52,59 +52,46 @@ def jukesCantor(branchLen):
                 St[i][j] = 1/4*(1-math.exp(-4*alpha*t))
     return St
 
+# after getting probablities from felsenstein() for each tree, compute
+# the termination step using a uniform background composition 
+# (i.e. q_a = .25 for all a in A,C,G,T)    
+def finalProb(plk):
+    total = 0
+    for k,v in list(plk.items()):
+        total += (v * .25)
+    print("P(data|tree) = " + str(total))
+
 def Main():
     global alphabet, rev_alphabet
     alphabet = {"A": 0, "C": 1, "G": 2, "T": 3}
     rev_alphabet = {0: "A", 1: "C", 2: "G", 3: "T"}
     
     # get trees from each file
-    print("------------------ Test 1 ------------------")
-    test = Phylo.read("test.txt", "newick")
-    test.rooted = True
-    plk = felsenstein(6, test.clade)
-    total = 0
-    composition = {'A': 0.2, 'C':0.3, 'G': 0.3, 'T': 0.2}
-    for k,v in list(plk.items()):
-        total += (v * composition[k])
-    print("P(data|tree) = " + str(total))
-    
-
     tree1 = Phylo.read("tree1.txt", "newick")
     tree1.rooted = True
-    #Phylo.draw_ascii(tree1)
 
     tree2 = Phylo.read("tree2.txt", "newick")
     tree2.rooted = True
-    #Phylo.draw_ascii(tree2)
-
+    
     tree3 = Phylo.read("tree3.txt", "newick")
     tree3.rooted = True
-    #Phylo.draw_ascii(tree3)
     
     root1 = tree1.clade
     root2 = tree2.clade
     root3 = tree3.clade
     
+    
     print("------------------ Tree 1 ------------------")
-    plk = felsenstein(6, root1)
-    total = 0
-    for k,v in list(plk.items()):
-        total += (v * .25)
-    print("P(data|tree) = " + str(total))
+    plk = felsenstein(root1)
+    finalProb(plk)
     
     print("\n\n------------------ Tree 2 ------------------")
-    plk = felsenstein(6, root2)
-    total = 0
-    for k,v in list(plk.items()):
-        total += (v * .25)
-    print("P(data|tree) = " + str(total))
+    plk = felsenstein(root2)
+    finalProb(plk)
     
     print("\n\n------------------ Tree 3 ------------------")
-    plk = felsenstein(6, root3)
-    total = 0
-    for k,v in list(plk.items()):
-        total += (v * .25)
-    print("P(data|tree) = " + str(total))
+    plk = felsenstein(root3)
+    finalProb(plk)
 
 if __name__ == '__main__':
     Main()
