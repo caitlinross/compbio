36777920279731c182d4d4561e067e567c157caf
Caitlin Ross <rossc3@rpi.edu>
2015-12-10 15:53:09 -0500

    finished assignment 8

--- assignment8/assignment8.m
+++ assignment8/assignment8.m
@@ -1,14 +1,21 @@
+% Author: Caitlin Ross
+% Due date: 12/15/15
+% CSCI 6971
+% Assignment 8
+
 % read data
 vals = textread('data.txt', '%f');
 
 % plot histogram of data
-hist(vals)
+figure(1)
+hist(vals, 20)
 xlabel('data values')
 ylabel('frequency')
 title('histogram of data values')
 saveas(gcf, 'hist.png')
 
 % qq plot of data
+figure(2)
 qqplot(vals)
 saveas(gcf, 'qq.png')
 
@@ -20,6 +27,7 @@ pd = makedist('Normal', 0, 1);
 pvals = 2*cdf(pd, -abs(vals));
 
 % plot histogram of p values
+figure(3)
 hist(pvals,20)
 xlabel('data values')
 ylabel('frequency')
@@ -34,6 +42,7 @@ for i=1:length(pvals_sort)
     newvals(i,1) = length(pvals_sort)*pvals_sort(i,1)*(1/fdr);
 end
 
+% find r, the cutoff point
 r=0;
 for i=1:length(newvals)
    if i < newvals(i)
@@ -44,14 +53,20 @@ end
 
 r = r - 1;
 p_cutoff = pvals_sort(r);
+fprintf('FDR cutoff = 0.05\n');
+fprintf('r = %d.\n', r);
+fprintf('p-value at FDR cutoff = %f\n', p_cutoff);
 
 totalp = 0;
-% # pvals less than 0.05
+% find # of pvals less than 0.05
 for i=1:length(pvals_sort)
    if pvals_sort(i) < 0.05
        totalp = totalp + 1;
    end
 end
+
+fprintf('number of p-values less than 0.05 is %d\n', totalp);
+fprintf('number of p-values less than %f is %d\n', p_cutoff, r);
 
 % plot raw data as points
 % indicate which are below FDR and which have p-value below 0.05
@@ -69,6 +84,7 @@ for i=1:length(pvals_sort)
        
 end
 
+figure(4)
 scatter(fdr_cut(:,1), fdr_cut(:,2), 'bo')
 hold on
 scatter(pval_cut(:,1), pval_cut(:,2), 'rx')
